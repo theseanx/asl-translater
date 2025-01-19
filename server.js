29adfb44fe7b0982630c5a53e9a31d42abdcc730
weixu <seanxu7788@gmail.com>
2025-01-18 18:54:37 -0800

    added server file

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const { exec } = require('child_process');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/translate', (req, res) => {
+    const text = req.body.text;
+
+    if (!text) {
+        return res.status(400).send('Text is required');
+    }
+
+    exec(`python3 -c "from sign_language_translator import Translator; print(Translator().translate('${text}'))"`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).send('Error translating text');
+        }
+        res.send(stdout);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
